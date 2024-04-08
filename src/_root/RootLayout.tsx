56cb12a29593062374339668b7098aa4678156f0
Samson <samsonjeyanthan5@gmail.com>
2024-04-08 19:45:33 +0530

    widgets and components

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,5 +1,17 @@
+import { Bottombar, LeftSidebar, Topbar } from "@/components/shared";
+import { Outlet } from "react-router-dom";
+
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return (
+    <main className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+      <Bottombar />
+    </main>
+  );
 };
 
 export default RootLayout;
---
+++ src/components/shared/index.ts
@@ -0,0 +1,3 @@
+export { default as Topbar } from "@/components/shared/Topbar";
+export { default as LeftSidebar } from "@/components/shared/LeftSidebar";
+export { default as Bottombar } from "@/components/shared/Bottombar";
